408539e8443ae462c72fbb72e135299062b6b939
Sergei Aleksandrov <sergei.a.aleks@gmail.com>
2026-03-17 20:27:38 +0100

    chore(tmux): Update role

--- roles/tmux/molecule/default/tests/test_default.py
+++ roles/tmux/molecule/default/tests/test_default.py
@@ -17,16 +17,16 @@ def test_smoke(host, user):
 @pytest.mark.parametrize(
     "os_name,os_codename,package_name,package_version",
     [
-        ("debian", "trixie", "tmux", "3.5"),
-        ("debian", "bookworm", "tmux", "3.3"),
+        ("debian", "trixie", "tmux", "3.3a"),
+        ("debian", "bookworm", "tmux", "3.3a"),
     ],
 )
 def test_package_is_installed(host, os_name, os_codename, package_name, package_version):
     host_os = host.system_info.distribution
     host_os_codename = host.system_info.codename
-    host_package = host.package(package_name)
 
     if host_os == os_name and os_codename == host_os_codename:
-        assert host_package.version.startswith(package_version)
+        cmd = host.run_test("tmux -V")
 
-    assert host_package.is_installed
+        assert cmd.rc == 0
+        assert cmd.stdout.startswith(f"tmux {package_version}\n")
